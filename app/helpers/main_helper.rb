9c0c768b33643884ec39a9d34940db22484949b1
qadirbf <870387339@qq.com>
2013-03-11 15:29:44 +0800

    修改

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,12 @@
+#encoding:utf-8
 module MainHelper
+
+  def auto_link(n)
+    ary = [["自动化", 1], ["信息化", 2], ["智能化", 3], ["节能及维保", 4]]
+    a = ary.select{|v| v[1] == n}.first
+    id = Category.where("name like ?", "%#{a[0]}%").first.try(:id)
+    return "/projects/case_list?cat=#{id}"
+  end
+
+
 end
