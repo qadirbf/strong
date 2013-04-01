828d44d9ab503e7b7411f29720ab76778776ba11
qadirbf <870387339@qq.com>
2013-04-01 15:45:47 +0800

    加入异常处理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 #encoding:utf-8
 class ApplicationController < ActionController::Base
 
+  rescue_from Exception do |exception|
+    Rails.logger.info '%'*100
+    Rails.logger.info exception.message
+    Emailer.deliver_test_email('870387339@qq.com', 'This is a test e-mail!')
+    Rails.logger.info '%'*100
+    render :file => "#{Rails.root}/public/500.html", :status => 500
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   validates_uniqueness_of :name, :message => "项目名称已经存在", :if => Proc.new{|p| p.name}
   validates_presence_of :project_date, :message => "项目时间不能为空"
   validates_presence_of :tech_info, :message => "请填写技术信息"
-  validates_presence_of :description, :message => "请填写项目描述"
+  #validates_presence_of :description, :message => "请填写项目描述"
   validates_presence_of :system_config, :message => "请填写系统配置"
 
   belongs_to :category
